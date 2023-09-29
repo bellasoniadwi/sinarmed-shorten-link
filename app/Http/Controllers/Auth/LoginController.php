dde6309230a0a8cd8b6e857ffb3e9593679dba1a
bellasoniadwi <bellasoniadwi@gmail.com>
2023-09-29 23:31:32 +0700

    Login berhasil, redirect ke dashboard

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = RouteServiceProvider::HOME;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+
+    public function showLoginForm()
+    {
+        return view('auth.login');
+    }
+
+    public function login(Request $request){
+        $input = $request->all();
+        $this->validate($request,[
+            'username' => 'required',
+            'password' => 'required'
+        ]);
+
+        if(Auth()->attempt(array('username'=>$input['username'], 'password'=>$input['password']))){
+                return redirect()->route('home');
+        } else {
+            return redirect()->route('login')->with('error', 'Login failed!');
+        }
+    }
+
+    public function logout(Request $request)
+    {
+        $this->guard()->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        if ($response = $this->loggedOut($request)) {
+            return $response;
+        }
+
+        return $request->wantsJson()
+            ? new JsonResponse([], 204)
+            : redirect('/');
+    }
+}
---
+++ resources/views/adminLayout/main.blade.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Admin || @yield('title')</title>
+  <link rel="shortcut icon" type="image/png" href="{{ asset('styleAdmin/images/logos/favicon.png')}}" />
+  <link rel="stylesheet" href="{{ asset('styleAdmin/css/styles.min.css')}}" />
+</head>
+
+<body>
+  <!--  Body Wrapper -->
+  <div class="page-wrapper" id="main-wrapper" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full"
+    data-sidebar-position="fixed" data-header-position="fixed">
+    @include('adminLayout.sidebar')
+    <!--  Main wrapper -->
+    <div class="body-wrapper">
+        @include('adminLayout.header')
+      <div class="container-fluid">
+        @yield('content')
+        @include('adminLayout.footer')
+      </div>
+    </div>
+
+    @yield('js')
+  </div>
+  <script src="{{ asset('styleAdmin/libs/jquery/dist/jquery.min.js')}}"></script>
+  <script src="{{ asset('styleAdmin/libs/bootstrap/dist/js/bootstrap.bundle.min.js')}}"></script>
+  <script src="{{ asset('styleAdmin/js/sidebarmenu.js')}}"></script>
+  <script src="{{ asset('styleAdmin/js/app.min.js')}}"></script>
+  <script src="{{ asset('styleAdmin/libs/apexcharts/dist/apexcharts.min.js')}}"></script>
+  <script src="{{ asset('styleAdmin/libs/simplebar/dist/simplebar.js')}}"></script>
+  <script src="{{ asset('styleAdmin/js/dashboard.js')}}"></script>
+</body>
+
+</html>
