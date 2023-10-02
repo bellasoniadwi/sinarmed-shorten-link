eda2d9c719ea0570139c19cb891e8e9ee9530d08
anjanidwilestari <ayianjani774@gmail.com>
2023-10-02 13:38:29 +0700

    responsive dropdown

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1046,6 +1046,25 @@
             box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
             z-index: 1;
         }
+
+        @media (max-width:768px) {
+            .dropdown-btn {
+                width: 559px;
+            }
+        }
+
+        @media (max-width:480px) {
+            .dropdown-btn {
+                width: 431px;
+            }
+        }
+
+        @media (max-width:400px) {
+            .dropdown-btn {
+                width: 359px;
+            }
+        }
+
     </style>
     <style>
     </style>
