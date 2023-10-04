7462767ce18b9d229713359e6ac37d26202559ed
bellasoniadwi <bellasoniadwi@gmail.com>
2023-10-04 10:16:38 +0700

    mengubah warna blur menjadi putih

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -10,7 +10,7 @@
     <link rel="manifest" href="{{ asset('styleUser/style/manifest.json') }}">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&amp;display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('styleUser/css/styles.css')}}" />
+    <link rel="stylesheet" href="{{ asset('styleUser/css/styles.css') }}" />
 </head>
 
 <body>
@@ -41,7 +41,8 @@
                             <path fill="#fff"
                                 d="M21.938,7.71a7.329,7.329,0,0,0-.456-2.394,4.615,4.615,0,0,0-1.1-1.694,4.61,4.61,0,0,0-1.7-1.1,7.318,7.318,0,0,0-2.393-.456C15.185,2.012,14.817,2,12,2s-3.185.012-4.29.062a7.329,7.329,0,0,0-2.394.456,4.615,4.615,0,0,0-1.694,1.1,4.61,4.61,0,0,0-1.1,1.7A7.318,7.318,0,0,0,2.062,7.71C2.012,8.814,2,9.182,2,12s.012,3.186.062,4.29a7.329,7.329,0,0,0,.456,2.394,4.615,4.615,0,0,0,1.1,1.694,4.61,4.61,0,0,0,1.7,1.1,7.318,7.318,0,0,0,2.393.456c1.1.05,1.472.062,4.29.062s3.186-.012,4.29-.062a7.329,7.329,0,0,0,2.394-.456,4.9,4.9,0,0,0,2.8-2.8,7.318,7.318,0,0,0,.456-2.393c.05-1.1.062-1.472.062-4.29S21.988,8.814,21.938,7.71Zm-1,8.534a6.351,6.351,0,0,1-.388,2.077,3.9,3.9,0,0,1-2.228,2.229,6.363,6.363,0,0,1-2.078.388C15.159,20.988,14.8,21,12,21s-3.159-.012-4.244-.062a6.351,6.351,0,0,1-2.077-.388,3.627,3.627,0,0,1-1.35-.879,3.631,3.631,0,0,1-.879-1.349,6.363,6.363,0,0,1-.388-2.078C3.012,15.159,3,14.8,3,12s.012-3.159.062-4.244A6.351,6.351,0,0,1,3.45,5.679a3.627,3.627,0,0,1,.879-1.35A3.631,3.631,0,0,1,5.678,3.45a6.363,6.363,0,0,1,2.078-.388C8.842,3.012,9.205,3,12,3s3.158.012,4.244.062a6.351,6.351,0,0,1,2.077.388,3.627,3.627,0,0,1,1.35.879,3.631,3.631,0,0,1,.879,1.349,6.363,6.363,0,0,1,.388,2.078C20.988,8.841,21,9.2,21,12S20.988,15.159,20.938,16.244Z">
                             </path>
-                            <path fill="#fff" d="M17.581,5.467a.953.953,0,1,0,.952.952A.954.954,0,0,0,17.581,5.467Z"></path>
+                            <path fill="#fff" d="M17.581,5.467a.953.953,0,1,0,.952.952A.954.954,0,0,0,17.581,5.467Z">
+                            </path>
                             <path fill="#fff"
                                 d="M12,7.073A4.927,4.927,0,1,0,16.927,12,4.932,4.932,0,0,0,12,7.073Zm0,8.854A3.927,3.927,0,1,1,15.927,12,3.932,3.932,0,0,1,12,15.927Z">
                             </path>
@@ -62,9 +63,9 @@
                     <a target="_blank" rel="noopener" data-testid="SocialIcon"
                         href="https://www.youtube.com/channel/UCwlvBU1w44b3CbZ2nk4gRsg"
                         aria-label="PT Sinarmed Jaya youtube" class="sc-eCssSg lbrsth sc-kstrdz btjemE">
-                        <svg enable-background="new 0 0 24 24" viewBox="0 0 24 24" class="sc-gKsewC hxdUPG" >
+                        <svg enable-background="new 0 0 24 24" viewBox="0 0 24 24" class="sc-gKsewC hxdUPG">
                             <title data-testid="svgTitle" id="title_0.45802239483822604">youtube</title>
-                            <path fill="#fff" 
+                            <path fill="#fff"
                                 d="M12,20.55c-.3,0-7.279-.006-9.115-.5A3.375,3.375,0,0,1,.5,17.665,29.809,29.809,0,0,1,0,12,29.824,29.824,0,0,1,.5,6.334,3.375,3.375,0,0,1,2.885,3.948c1.836-.492,8.819-.5,9.115-.5s7.279.006,9.115.5A3.384,3.384,0,0,1,23.5,6.334,29.97,29.97,0,0,1,24,12a29.97,29.97,0,0,1-.5,5.666,3.384,3.384,0,0,1-2.388,2.386C19.279,20.544,12.3,20.55,12,20.55Zm0-16.1c-.072,0-7.146.006-8.857.464A2.377,2.377,0,0,0,1.464,6.593,29.566,29.566,0,0,0,1,12a29.566,29.566,0,0,0,.464,5.407,2.377,2.377,0,0,0,1.679,1.679c1.711.458,8.785.464,8.857.464s7.146-.006,8.857-.464a2.377,2.377,0,0,0,1.679-1.679A29.66,29.66,0,0,0,23,12a29.66,29.66,0,0,0-.464-5.407h0a2.377,2.377,0,0,0-1.679-1.679C19.146,4.456,12.071,4.45,12,4.45ZM9.7,15.95a.5.5,0,0,1-.5-.5V8.55a.5.5,0,0,1,.75-.433l5.975,3.45a.5.5,0,0,1,0,.866L9.95,15.883A.5.5,0,0,1,9.7,15.95Zm.5-6.534v5.168L14.675,12Z">
                             </path>
                         </svg></a>
@@ -95,88 +96,90 @@
                 <div class="page-item-wrap relative">
                     <div class="page-item flex-both-center absolute"></div>
                     <a target="_blank" class="page-item-each py-10 flex-both-center"
-                        href="https://api.whatsapp.com/send?phone=628113387053" data-id="262840" data-type="page_item">
+                        href="https://api.whatsapp.com/send?phone=628113387053" data-id="262840"
+                        data-type="page_item">
                         <img class="link-each-image" data-src="{{ asset('styleUser/style/wa.png') }}"
                             alt="Whatsapp Sinarmed" />
                         <span class=" item-title text-center">WHATSAPP</span>
                     </a>
                 </div>
-                
+
                 <div class="page-item-wrap relative">
                     <div class="page-item flex-both-center absolute"></div>
-                    <a target="_blank" class="page-item-each py-10 flex-both-center"
-                        href="https://www.sinarmed.com/" data-id="261652" data-type="page_item">
+                    <a target="_blank" class="page-item-each py-10 flex-both-center" href="https://www.sinarmed.com/"
+                        data-id="261652" data-type="page_item">
                         <img class="link-each-image" data-src="{{ asset('styleAdmin/images/logos/sinarmed.png') }}"
                             alt="Website Sinarmed" />
                         <span class=" item-title text-center">SINARMED</span>
                     </a>
                 </div>
 
-                    <div class="page-item-wrap relative">
-                        <div class="page-item flex-both-center absolute"></div>
-                        <a target="_blank" class="page-item-each py-10 flex-both-center"
-                            href="https://pahsco.co.id/" data-id="262840" data-type="page_item">
-                            <img class="link-each-image" data-src="{{ asset('styleUser/style/pahsco.png') }}"
-                                alt="Pahsco" />
-                            <span class=" item-title text-center">PAHSCO</span>
-                        </a>
-                    </div>
-                
+                <div class="page-item-wrap relative">
+                    <div class="page-item flex-both-center absolute"></div>
+                    <a target="_blank" class="page-item-each py-10 flex-both-center" href="https://pahsco.co.id/"
+                        data-id="262840" data-type="page_item">
+                        <img class="link-each-image" data-src="{{ asset('styleUser/style/pahsco.png') }}"
+                            alt="Pahsco" />
+                        <span class="item-title text-center">PAHSCO</span>
+                    </a>
+                </div>
+
 
                 <div class="dropdown">
                     <button class="dropdown-btn">EKATALOG</button>
                     <img class="link-each-image" data-src="{{ asset('styleUser/style/e-katalog.png') }}"
-                            alt="Katalog Sinarmed" />
+                        alt="Katalog Sinarmed" />
                     <div class="dropdown-content">
                         @foreach ($ekatalog as $ek)
-                                <div class="page-item flex-both-center absolute"></div>
-                                <a target="_blank" class="page-item-each py-10 flex-both-center" href="{{ $ek->link_product }}"
-                                    data-id="261652" data-type="page_item">
-                                    <img class="link-each-image" data-src="{{ asset('storage/' . $ek->gambar_product) }}"
-                                        alt="{{ $ek->nama_product }}" />
-                                    <span class=" item-title text-center">{{ $ek->nama_product }}</span>
-                                </a>
+                            <div class="page-item flex-both-center absolute"></div>
+                            <a target="_blank" class="page-item-each py-10 flex-both-center"
+                                href="{{ $ek->link_product }}" data-id="261652" data-type="page_item">
+                                <img class="link-each-image" data-src="{{ asset('storage/' . $ek->gambar_product) }}"
+                                    alt="{{ $ek->nama_product }}" />
+                                <span class=" item-title text-center">{{ $ek->nama_product }}</span>
+                            </a>
                         @endforeach
                     </div>
                 </div>
 
                 <div class="page-item-wrap relative">
-                <div class="css-x3hr8h">
-                    <div style="border-radius:30px;border:0.5px solid #FFFFFF" class="css-1goyi81">
-                        <div class="css-139awdm">
-                            <div color="#FFFFFF" style="border-bottom:1px solid #FFFFFF" class="css-18dalow e1cu2qz1">
-                                <picture visibility="visible" class="css-18ikrgg en27bju17">
-                                    <source srcset="https://super-content.s3-ap-southeast-1.amazonaws.com/users/98269/icons/62d65be605b8766aa957eb1b">
+                    <div class="css-x3hr8h">
+                        <div style="border-radius: 8px; border: 0.5px solid #FFFFFF; background-color: #FFFFFF;" class="css-1goyi81">
+                            <div class="css-139awdm">
+                                <div color="#FFFFFF" style="border-bottom: 1px solid #b2aeae" class="css-18dalow e1cu2qz1">
+                                    <picture visibility="visible" class="css-18ikrgg en27bju17">
+                                        <source srcset="https://super-content.s3-ap-southeast-1.amazonaws.com/users/98269/icons/62d65be605b8766aa957eb1b">
                                         <img visibility="visible" loading="lazy" class="css-1d3ess8 en27bju18">
-                                </picture>
-                                <h2>PRODUK DALAM NEGERI</h2>
+                                    </picture>
+                                    <h2><span class="text-center">PRODUK DALAM NEGERI</span></h2>
+                                </div>
+                    
+                                {{-- <div color="#FFFFFF" class="css-y4awvf e1cu2qz0">
+                                    <h3>KHUSUS PRODUK AKD</h3>
+                                </div> --}}
                             </div>
-                            
-                            {{-- <div color="#FFFFFF" class="css-y4awvf e1cu2qz0">
-                                <h3>KHUSUS PRODUK AKD</h3>
-                            </div> --}}
-                        </div>
-                        
-                        <div display="grid" class="css-37t1ki e1axnq4j3">
-                            @foreach ($dalamnegeri as $dn)
-                                <a href="{{ $dn->link_product }}">
-                                    <div class="css-1fqugoq eayjb5n4">
-                                        <img alt="Medical Gas Alarm System" src="{{ asset('storage/' . $dn->gambar_product) }}">
-                                        <div font-size="14px" class="css-1bmv740 eayjb5n2">
-                                            <span>{{ $dn->nama_product }}</span>
+                    
+                            <div display="grid" class="css-37t1ki e1axnq4j3">
+                                @foreach ($dalamnegeri as $dn)
+                                    <a href="{{ $dn->link_product }}">
+                                        <div class="css-1fqugoq eayjb5n4">
+                                            <img alt="Medical Gas Alarm System" src="{{ asset('storage/' . $dn->gambar_product) }}">
+                                            <div font-size="14px" class="css-1bmv740 eayjb5n2">
+                                                <span>{{ $dn->nama_product }}</span>
+                                            </div>
                                         </div>
-                                    </div>
-                                </a>
-                            @endforeach
+                                    </a>
+                                @endforeach
+                            </div>
                         </div>
                     </div>
+                    
                 </div>
-            </div>
             </div>
         </div>
     </div>
 
-    
+
 </body>
 
 <script>
